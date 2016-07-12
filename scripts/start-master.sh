98a40a5b8f2ef5278ead75880a43a08e51c326f6
Thejas <thejas.p.murthy@gmail.com>
2016-07-12 12:48:51 +0200

    Fix spark master binding to 127.0.0.1:7077
    need to use docker provided IP, to enable auto discovery from slaves

--- scripts/start-master.sh
+++ scripts/start-master.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
-export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
+export SPARK_MASTER_IP=`ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }'`
+export SPARK_LOCAL_IP=`ifconfig eth0 | grep "inet " | awk -F'[: ]+' '{ print $4 }'`
 /usr/local/spark/sbin/start-master.sh --properties-file /spark-defaults.conf -i $SPARK_LOCAL_IP "$@"
 /bin/bash
